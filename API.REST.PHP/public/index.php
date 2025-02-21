3a8676d70a7be816a6f0a4395d47ef963e683543
Santiago Plazas <127706955+wrongzeta@users.noreply.github.com>
2025-02-21 20:46:13 +0000

    Agregada autenticación y ajustes en la API REST

--- API.REST.PHP/public/index.php
+++ API.REST.PHP/public/index.php
@@ -2,21 +2,3 @@
 header('Location: login.php');
 exit();
 ?>
-```
-
-public/login.php
-```php
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-<body>
-    <form action="auth.php" method="post">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>
